e6b816a7bae138005dfc4f8d7945004a098bd356
Danila <asqdanil@yandex.ru>
2024-08-22 16:12:54 +0300

    fixed | update

--- script/index.js
+++ script/index.js
@@ -8,83 +8,42 @@ function fetchAndUpdateContent(url) {
         .then(response => response.json())
         .then(data => {
             const contentElement = document.getElementById('content');
+            const emptyListExists = document.querySelector('.empty-list') !== null;
 
-            // Скрываем элемент загрузки после успешной загрузки данных
-            if (loadingElement) {
-                loadingElement.classList.add('hidden');
-            }
-
-            // Сначала определяем, какие серверы новые, чтобы добавить их
-            const newServers = new Map(Object.entries(data));
-            const serversToRemove = [];
-
-            // Проверяем, какие серверы удалены
-            currentServers.forEach((value, key) => {
-                if (!newServers.has(key)) {
-                    serversToRemove.push(key);
+            if (Object.entries(data).length === 0) {
+                if (loadingElement) {
+                    loadingElement.classList.add('hidden');
                 }
-            });
-
-            // Удаляем карточки серверов
-            serversToRemove.forEach(port => {
-                const blockToRemove = document.querySelector(`.block[data-port="${port}"]`);
-                if (blockToRemove) {
-                    blockToRemove.classList.remove('visible');
-                    blockToRemove.classList.add('invisible');
-                    setTimeout(() => blockToRemove.remove(), 300);
-                    currentServers.delete(port);
-                }
-            });
-
-            // Добавляем или обновляем карточки серверов
-            for (const [port, serverInfo] of newServers.entries()) {
-                if (!currentServers.has(port)) {
-                    const serverBlock = document.createElement('div');
-                    serverBlock.className = 'block invisible';
-                    serverBlock.setAttribute('data-port', port);
-
-                    const imageUrl = `../image/${serverInfo.map}.png`;
-                    const pingMs = String(Math.floor(parseFloat(serverInfo.ping) * 1000));
-
-                    let pingClass = '';
-                    if (parseInt(pingMs) > 80) {
-                        pingClass = 'ping-red';
-                    } else if (parseInt(pingMs) > 50) {
-                        pingClass = 'ping-yellow';
-                    } else {
-                        pingClass = 'ping-green';
-                    }
-
-                    serverBlock.innerHTML = `
-                        <div class="image" style="background-image: 
-                            linear-gradient(to bottom, rgba(255, 255, 255, 0), rgba(0, 0, 0, 0.986) 80%),
-                            url('${imageUrl}');"></div>
-                        <div class="ping title-text ${pingClass}">${pingMs}</div>
-                        <div class="people title-text">${serverInfo.now_players}/${serverInfo.max_players}</div>
-                        <div class="play">
-                            <div class="play-icon" data-connect-server="${serverInfo.connect_server}">
-                                <img src="../image/icon/play-icon.png" alt="Play-icon">
-                            </div>
-                            <div class="circle-icon">
-                                <img src="../image/icon/circle.png" alt="circle-icon">
-                            </div>
+                console.log('Список серверов пуст');
+                if (!emptyListExists) {
+                    const emptyBlock = document.createElement('div');
+                    emptyBlock.className = 'empty-server-list';
+                    emptyBlock.innerHTML = `
+                        <div class="empty-list">
+                            <img src="../image/icon/empty.png" alt="Empty server" class="image-empty">
+                            <div class="refresh-instruction title-text">CTRL + R</div>
                         </div>
-                        <div class="server_name title-text">${serverInfo.server_name}</div>
-                        <div class="map title-text">${serverInfo.map}</div>
                     `;
-                    contentElement.appendChild(serverBlock);
-                    
-                    setTimeout(() => {
-                        serverBlock.classList.remove('invisible');
-                        serverBlock.classList.add('visible');
-                    }, 50);
-
-                    currentServers.set(port, serverInfo);
-                } else {
-                    currentServers.set(port, serverInfo);
+                    contentElement.appendChild(emptyBlock);
                 }
+            } else {
+                // Если данные серверов не пустые, убираем элемент с пустым списком, если он есть
+                if (emptyListExists) {
+                    const emptyBlock = document.querySelector('.empty-list').parentNode;
+                    if (emptyBlock) {
+                        emptyBlock.remove();
+                    }
+                }
+
+                // Скрываем элемент загрузки после успешной загрузки данных
+                if (loadingElement) {
+                    loadingElement.classList.add('hidden');
+                }
+
+                // Обновление карточек серверов
+                updateServerCards(currentServers, data, contentElement);
             }
-            
+
             const playIcons = document.querySelectorAll('.play-icon');
             playIcons.forEach(icon => {
                 icon.addEventListener('click', function() {
@@ -102,6 +61,80 @@ function fetchAndUpdateContent(url) {
             </div>
             `;
         });
+}
+
+function updateServerCards(currentServers, data, contentElement) {
+    // Работаем с текущими серверами и новыми данными
+    // Находим новые и удаляем старые серверы
+
+    const newServers = new Map(Object.entries(data));
+    const serversToRemove = [];
+
+    currentServers.forEach((value, key) => {
+        if (!newServers.has(key)) {
+            serversToRemove.push(key);
+        }
+    });
+
+    // Удаление старых серверов
+    serversToRemove.forEach(port => {
+        const blockToRemove = document.querySelector(`.block[data-port="${port}"]`);
+        if (blockToRemove) {
+            blockToRemove.classList.remove('visible');
+            blockToRemove.classList.add('invisible');
+            setTimeout(() => blockToRemove.remove(), 300);
+            currentServers.delete(port);
+        }
+    });
+
+    // Добавление и обновление новых серверов
+    for (const [port, serverInfo] of newServers.entries()) {
+        if (!currentServers.has(port)) {
+            const serverBlock = document.createElement('div');
+            serverBlock.className = 'block invisible';
+            serverBlock.setAttribute('data-port', port);
+
+            const imageUrl = `../image/${serverInfo.map}.png`;
+            const pingMs = String(Math.floor(parseFloat(serverInfo.ping) * 1000));
+
+            let pingClass = '';
+            if (parseInt(pingMs) > 80) {
+                pingClass = 'ping-red';
+            } else if (parseInt(pingMs) > 50) {
+                pingClass = 'ping-yellow';
+            } else {
+                pingClass = 'ping-green';
+            }
+
+            serverBlock.innerHTML = `
+                <div class="image" style="background-image: 
+                    linear-gradient(to bottom, rgba(255, 255, 255, 0), rgba(0, 0, 0, 0.986) 80%),
+                    url('${imageUrl}');"></div>
+                <div class="ping title-text ${pingClass}">${pingMs}</div>
+                <div class="people title-text">${serverInfo.now_players}/${serverInfo.max_players}</div>
+                <div class="play">
+                    <div class="play-icon" data-connect-server="${serverInfo.connect_server}">
+                        <img src="../image/icon/play-icon.png" alt="Play-icon">
+                    </div>
+                    <div class="circle-icon">
+                        <img src="../image/icon/circle.png" alt="circle-icon">
+                    </div>
+                </div>
+                <div class="server_name title-text">${serverInfo.server_name}</div>
+                <div class="map title-text">${serverInfo.map}</div>
+            `;
+            contentElement.appendChild(serverBlock);
+            
+            setTimeout(() => {
+                serverBlock.classList.remove('invisible');
+                serverBlock.classList.add('visible');
+            }, 50);
+
+            currentServers.set(port, serverInfo);
+        } else {
+            currentServers.set(port, serverInfo);
+        }
+    }
 }
 
 function join_to_server(connectServer) {
