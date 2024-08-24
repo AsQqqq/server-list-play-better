68d61ac832388d5e2b56818fa0414fc298b3bf12
Danila <asqdanil@yandex.ru>
2024-08-24 15:59:28 +0300

    Fix and update

--- index.js
+++ index.js
@@ -2,30 +2,57 @@ const path = require('path');
 const url = require('url');
 const { app, BrowserWindow, ipcMain } = require('electron');
 const packageJson = require('./package.json');
-const { updateElectronApp, UpdateSourceType } = require('update-electron-app');
+const { updateElectronApp } = require('update-electron-app');
+const { autoUpdater } = require('electron-updater');
+const log = require('electron-log');
 
+// Настройка логов
+const logPath = path.join(__dirname, 'log.txt');
+log.transports.file.resolvePath = () => logPath;
+log.transports.file.level = 'debug';
+log.transports.file.format = '[{y}-{m}-{d} {h}:{i}:{s}.{ms}] [{level}] {text}';
 
-// const { autoUpdater } = require('electron-updater');
+log.transports.console.level = 'debug';
+log.transports.console.format = '[{y}-{m}-{d} {h}:{i}:{s}.{ms}] [{level}] {text}';
 
-// autoUpdater.checkForUpdatesAndNotify();
+log.info(`Logging to file: ${logPath}`);
 
-// autoUpdater.on('update-available', (info) => {
-//     console.log(`Update available: ${info.version}`);
-// });
+function logEvent(message) {
+    console.log(message); // Дублирование в консоль
+    log.info(message);    // Логирование в файл и в консоль
+}
 
-// autoUpdater.on('update-downloaded', (info) => {
-//     console.log('Update downloaded');
-//     autoUpdater.quitAndInstall();
-// });
+logEvent(`NODE_ENV: ${process.env.NODE_ENV}`);
 
+if (process.env.NODE_ENV === 'production') {
+    updateElectronApp({
+        repo: 'AsQqqq/server-list-play-better',
+        updateInterval: '1 hour',
+        logger: log
+    });
+} else {
+    logEvent('App is in development mode; skipping update checks.');
+}
+
+autoUpdater.checkForUpdatesAndNotify();
+
+autoUpdater.on('update-available', (info) => {
+    logEvent(`Update available: version ${info.version}`);
+});
+
+autoUpdater.on('update-downloaded', (info) => {
+    logEvent('Update downloaded, installing...');
+    autoUpdater.quitAndInstall();
+});
 
 function createWindow() {
+    logEvent('Creating main application window');
     const win = new BrowserWindow({
         width: 800,
         height: 600,
         minHeight: 400,
         minWidth: 720,
-        title: 'Список серверов CS2',
+        title: 'CS2 Server List',
         frame: false,
         icon: path.join(__dirname, 'icon.png'),
         webPreferences: {
@@ -35,10 +62,9 @@ function createWindow() {
             devTools: true
         }
     });
-    // win.webContents.openDevTools();
-
 
     win.webContents.on('did-finish-load', () => {
+        logEvent('Main window loaded');
         win.webContents.send('app-info', {
             version: packageJson.version,
             server_version: packageJson.server_version,
@@ -47,43 +73,53 @@ function createWindow() {
     });
 
     ipcMain.on('minimize-window', () => {
+        logEvent('Window minimized');
         win.minimize();
     });
 
-    ipcMain.on('maximize-window', (event, arg) => {
+    ipcMain.on('maximize-window', () => {
         if (!win.isMaximized()) {
+            logEvent('Window maximized');
             win.maximize();
         } else {
+            logEvent('Window unmaximized from maximized state');
             win.unmaximize();
         }
     });
 
     app.on('ready', () => {
-        ipcMain.on('get-state-window', (event, arg) => {
+        ipcMain.on('get-state-window', (event) => {
+            logEvent('Window state request');
             event.returnValue = {
                 isMaximized: win.isMaximized()
-            }
+            };
         });
     });
 
     ipcMain.on('close-window', () => {
+        logEvent('Window closed');
         win.close();
     });
 
-    // Загрузка страницы загрузки
+    logEvent('Loading design page');
     win.loadFile('design/index.html');
 }
 
-app.whenReady().then(createWindow);
+app.whenReady().then(() => {
+    logEvent('App ready, creating window');
+    createWindow();
+});
 
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
+        logEvent('All windows closed, quitting app');
         app.quit();
     }
 });
 
 app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
+        logEvent('App activated, creating new window');
         createWindow();
     }
 });
