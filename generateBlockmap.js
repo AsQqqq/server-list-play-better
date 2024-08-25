c108bfc3b1162cafd8e321d15fcefea778972102
Danila <asqdanil@yandex.ru>
2024-08-26 02:05:56 +0300

    fix

---
+++ generateBlockmap.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const path = require('path');
+
+const distDir = path.join(__dirname, 'dist');
+
+// Пример: создаем dummy файл блокмапа
+const blockmapFile = path.join(distDir, 'your-app.blockmap');
+
+const blockmapContent = { /* данные блокмапа */ };
+fs.writeFileSync(blockmapFile, JSON.stringify(blockmapContent, null, 2));
+console.log("Blockmap создан успешно в директории dist.");
