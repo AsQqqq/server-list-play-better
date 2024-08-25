9590dc8ef1cad6bc6beaf06ca198b7b0e43dd0a2
Danila <asqdanil@yandex.ru>
2024-08-26 02:02:10 +0300

    fix

--- createLatestJson.js
+++ createLatestJson.js
@@ -1,10 +1,15 @@
 const fs = require('fs');
+const path = require('path');
 
-const data = {
-  version: '0.1.9-beta',
-  description: 'Список серверов Counter Strike 2',
-  date: new Date().toISOString(),
-};
+const distDir = path.join(__dirname, 'dist');
 
-fs.writeFileSync('dist/latest.json', JSON.stringify(data, null, 2));
-console.log('latest.json file has been created');
+// Проверьте существование директории, и если её нет, создайте её
+if (!fs.existsSync(distDir)){
+    fs.mkdirSync(distDir, { recursive: true });
+}
+
+const data = { /* ваши данные */ };
+const jsonPath = path.join(distDir, 'latest.json');
+
+fs.writeFileSync(jsonPath, JSON.stringify(data, null, 2));
+console.log("latest.json создан успешно в директории dist.");
